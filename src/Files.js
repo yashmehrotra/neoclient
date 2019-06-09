80e8b0af507800cafbd86840fbabe3152ac37a40
Yash Mehrotra <yashmehrotra95@gmail.com>
2019-06-09 11:31:41 +0530

    Image preview (#1)

--- src/Files.js
+++ src/Files.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import download from 'downloadjs';
+import Modal from 'react-modal';
+import ImageGallery from 'react-image-gallery';
 import './Files.css';
 
+Modal.setAppElement(document.getElementById('root'));
 class Files extends Component {
     constructor(props) {
         super(props);
         this.state = {
             error: null,
             isLoaded: false,
-            items: []
+            items: [],
+            images: [],
+            isModalOpen: false
         };
         this.uploadBtn = React.createRef();
+
         this.uploadFiles = this.uploadFiles.bind(this);
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+
         this.host = 'https://neo.yashmehrotra.com';
+        this.pattern = /([.|\w|\s|-])*\.(?:jpg|gif|png)/g;
+        this.images = [];
+    }
+
+    openModal() {
+        this.setState({ isModalOpen: true });
+    }
+
+    closeModal() {
+        this.setState({ isModalOpen: false });
     }
 
     uploadFiles() {
@@ -38,6 +57,19 @@ class Files extends Component {
         .catch(error => console.log(error));
     }
 
+    handleFileClick(file) {
+        if (this.pattern.test(file.name)) {
+            this.images = this.state.images.map(file => {
+                const path = `https://neo.yashmehrotra.com/api/v1/get?path=${file.full_path}`;
+                return({
+                    original: path,
+                    thumbnail: path
+                });
+            })
+            this.openModal();
+        }
+    }
+
     componentDidMount() {
         this.getFiles(this.props.location.pathname);
     }
@@ -55,9 +87,11 @@ class Files extends Component {
             .then(res => res.json())
             .then(
                 (result) => {
+                    const images = result.fs_objects.filter(file => this.pattern.test(file.name));
                     this.setState({
                         isLoaded: true,
                         items: result.fs_objects,
+                        images
                     });
                 },
                 (error) => {
@@ -89,14 +123,32 @@ class Files extends Component {
                                             <img src="/folder.jpg" alt="folder" className="image-hai" /> {f.name}
                                         </Link>
                                     ) : (
-                                        <div>
-                                            <img src="/file.jpg" alt="file" className="image-hai" />{f.name} <small>{f.human_size}</small><button onClick={() => this.downloadFile(`${pathname}/${f.name}`)} className="download-btn">Download</button>
+                                        <div onClick={() => this.handleFileClick(f)}>
+                                            <img src="/file.jpg" alt="file" className="image-hai" />
+                                            {f.name} <small>{f.human_size}</small>
+                                            <button 
+                                                onClick={() => this.downloadFile(`${pathname}/${f.name}`)} 
+                                                className="download-btn"
+                                            >
+                                                Download
+                                            </button>
                                         </div>
                                     )}
                                 </li>
                             )) : <li className="text-center">Folder Empty</li>
                         }
                     </ul>
+                    <Modal
+                        isOpen={this.state.isModalOpen}
+                        onRequestClose={this.closeModal}
+                        contentLabel="Image Modal"
+                        className="modal"
+                    >
+                        <ImageGallery 
+                            items={this.images}
+                            showPlayButton={false}
+                        />
+                    </Modal>
                 </div>
             );
         }
